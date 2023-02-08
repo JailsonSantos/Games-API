8f11cc772bf9d19cd51b121876ee4809063ea490
JailsonSantos <jailson.ads007@gmail.com>
2023-02-08 11:09:12 -0300

    Config: Install and config Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send("hello World!")
+  res.send('hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log("listening on port " + PORT)
+  console.log('listening on port ' + PORT);
 });
